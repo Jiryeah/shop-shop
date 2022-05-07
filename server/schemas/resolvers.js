c30ac8754d71d41a697325cc6a3b1c25b0c9fbc6
Chidori <business.faissal@outlook.com>
2022-05-07 00:25:41 -0500

    added checkout method to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -57,25 +57,37 @@ const resolvers = {
       const { products } = await order.populate('products');
 
       const line_items = [];
+
       for (let i = 0; i < products.length; i++) {
         // generate product id
         const product = await stripe.products.create({
           name: products[i].name,
-          description: products[i],
-          description,
+          description: products[i].description,
         });
+
         // generate price id using the product id
         const price = await stripe.prices.create({
           product: product.id,
           unit_amount: products[i].price * 100,
           currency: 'usd',
         });
+
         // add price id to the line items array
         line_items.push({
           price: price.id,
           quantity: 1,
         });
       }
+
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items,
+        mode: 'payment',
+        success_url: 'https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
+        cancel_url: 'https://example.com/cancel',
+      });
+
+      return { session: session.id };
     },
   },
   Mutation: {
