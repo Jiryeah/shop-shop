703ce0b9b9b6cc37c044bf67d544b0a0003a6fc2
Faissal <faissal.shabani@ideaentity.com>
2024-03-11 14:41:30 -0500

    new build files

---
+++ client/build/precache-manifest.1394c341dd0045a95b6b511754df33f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2ddfd8991405dffbc165f381aa7381d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b58106fe702c959b379",
+    "url": "/static/css/main.e213c893.chunk.css"
+  },
+  {
+    "revision": "b0feed86eed8f2fb3f44",
+    "url": "/static/js/2.2325f18f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.2325f18f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b58106fe702c959b379",
+    "url": "/static/js/main.164681c6.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.164681c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
